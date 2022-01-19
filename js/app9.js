2ef3590e3622bbd0616512c319bdd56a8d59282f
Sergii <strizgsergey@gmail.com>
2022-01-19 22:19:17 +0200

    +

--- js/app9.js
+++ js/app9.js
@@ -9,5 +9,24 @@ canvas.height = window.innerHeight;
 // ===============
 
 ctx.moveTo(50, 50);
-ctx.quadraticCurveTo(100, 150, 300, 50);
+ctx.quadraticCurveTo(175, 250, 300, 50);
 ctx.stroke();
+// ctx.closePath();
+
+// ctx.beginPath();
+ctx.moveTo(50, 400);
+ctx.quadraticCurveTo(175, 250, 300, 400);
+ctx.stroke();
+
+// ctx.moveTo(190, 280);
+// ctx.quadraticCurveTo(200, 300, 210, 280);
+// ctx.stroke();
+
+ctx.moveTo(290, 600);
+ctx.quadraticCurveTo(200, 300, 210, 280);
+ctx.stroke();
+
+// ctx.beginPath();
+// ctx.moveTo(190, 280);
+// ctx.bezierCurveTo(190, 280, 200, 300, 210, 320);
+// ctx.stroke();
